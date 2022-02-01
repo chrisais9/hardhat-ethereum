ec3944b2297978552b6234abc6a73a0d5f6cf35e
chrisais9 <chrisais9@naver.com>
2022-02-02 01:38:30 +0900

    nft mint 하는 메소드 추가

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -4,7 +4,35 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Nft is ERC721 {
-    constructor(string memory name, string memory symbol)
-        ERC721(name, symbol)
-    {}
+    uint256 private _currentTokenId;
+    mapping(uint256 => string) private _tokenURIs;
+
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+        _currentTokenId = 0;
+    }
+
+    /**
+     * @dev Mints a token to an address with a tokenURI.
+     * @param _to address of the future owner of the token
+     */
+    function mintTo(address _to) public {
+        uint256 newTokenId = _getNextTokenId();
+        _mint(_to, newTokenId);
+        _incrementTokenId();
+    }
+
+    /**
+     * @dev calculates the next token ID based on value of _currentTokenId
+     * @return uint256 for the next token ID
+     */
+    function _getNextTokenId() private view returns (uint256) {
+        return _currentTokenId + 1;
+    }
+
+    /**
+     * @dev increments the value of _currentTokenId
+     */
+    function _incrementTokenId() private {
+        _currentTokenId++;
+    }
 }
